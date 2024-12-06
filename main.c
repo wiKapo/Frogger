11b713284a9b7548a5d3f3d4edcd78cc6d5b2efa
wikapo <wiktor200polanowski@gmail.com>
2024-12-06 23:55:36 +0100

    add a game screen
    for now only ground is showing up. Progress...

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include "time.h"
 #include "window.h"
+#include "game.h"
 
 int main() {
-    game_t game = Start();
-    screen_t screen = game.mainwin;
-    WINDOW* win = screen.win;
+    //read config from file
+    config_t *config = read_config_file("config.txt");
+
+    game_t game = Start(config);
+    screen_t screen = game.mainscr;
+    WINDOW *win = screen.win;
 
     int play = 1;
 
@@ -16,23 +20,27 @@ int main() {
         if (!state)
             play = 0;
 
-        long starttime = GetTime() - 50000;
+        long starttime = GetTime();
 
         while (state) {
             switch (state) {
-                case 9: break;
-                    //
+                case 1: break;
+                //
                 default: break;
             }
-                //frog_move();
-                //enemy_move();
+            StartGame(config, game);
+            //frog_move();
+            //enemy_move();
 
 
             long gametime = GetTime() - starttime;
 
-            mvwprintw(win, 0, screen.width - 12, "[%02ld:%02ld.%02ld]", gametime / 1000 / 60, (gametime/ 1000)%60, gametime % 1000);
+            mvwprintw(win, 0, screen.width - 12, "[%02ld:%02ld.%02ld]",
+                      gametime / 1000 / 60,
+                      (gametime / 1000) % 60,
+                      gametime % 1000);
             wrefresh(win);
-            timeout(125);
+            timeout(1000/30);
             if (getch() == 'q')
                 state = 0;
         }
