423ff3b7e5e853703b42378a86018780668ffc9f
wikapo <wiktor200polanowski@gmail.com>
2024-12-06 23:53:27 +0100

    add new colors

--- colors.c
+++ colors.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 void StartColor() {
-    if(has_colors() == FALSE) {
+    if (has_colors() == FALSE) {
         endwin();
         printf("Your terminal does not support color\n");
         exit(1);
@@ -11,11 +11,20 @@ void StartColor() {
     start_color();
     init_color(COLOR_BLACK, 0, 0, 0);
     init_color(COLOR_GREEN, 0, 750, 0);
+    init_color(COLOR_DARK_GREEN, 0, 500, 0);
+    init_color(COLOR_WHITE, 900, 900, 900);
 
     init_color(COLOR_LIGHT_GRAY, 609, 609, 609);
     init_color(COLOR_GRAY, 330, 330, 330);
     init_color(COLOR_DARK_GRAY, 100, 100, 100);
+    init_color(COLOR_DARKER_GRAY, 50, 50, 50);
 
     init_pair(1, COLOR_GREEN, COLOR_DARK_GRAY);
     init_pair(2, COLOR_WHITE, COLOR_DARK_GRAY);
+
+    //COLORS FOR GROUND
+    init_pair(10, COLOR_WHITE, COLOR_DARK_GREEN); //GRASS
+    init_pair(11, COLOR_WHITE, COLOR_DARKER_GRAY); //ROAD
+    init_pair(12, COLOR_WHITE, COLOR_CYAN); //WATER
+    init_pair(19, COLOR_WHITE, COLOR_RED); //FINISH
 }
--- colors.h
+++ colors.h
@@ -1,9 +1,11 @@
 #ifndef COLORS_H
 #define COLORS_H
 
-#define COLOR_LIGHT_GRAY   8
-#define COLOR_GRAY    9
-#define COLOR_DARK_GRAY    10
+#define COLOR_GRAY    8
+#define COLOR_DARK_GRAY    16
+#define COLOR_DARKER_GRAY    17
+#define COLOR_DARK_GREEN    18
+#define COLOR_LIGHT_GRAY   32
 
 void StartColor();
 
