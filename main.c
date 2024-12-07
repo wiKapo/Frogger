af40722ddc9fc98d39720926c0730b70b63c84f7
wikapo <wiktor200polanowski@gmail.com>
2024-12-07 23:18:24 +0100

    main.c split :)

--- main.c
+++ main.c
@@ -5,15 +5,14 @@
 #define CONFIG_FILENAME     "config.txt"
 #define FPS                 30
 
+void GameLoop(screen_t mainscr, const game_t *game, const config_t *config);
+
 int main() {
     //read config from file
     const config_t *config = ReadConfigFile(CONFIG_FILENAME);
 
     const game_screen_t game_screen = Start(config);
     const screen_t mainscr = game_screen.mainscr;
-    WINDOW *mainwin = mainscr.win;
-    const screen_t statscr = game_screen.statscr;
-    WINDOW *statwin = statscr.win;
 
     //MAIN LOOP
     while (1) {
@@ -21,7 +20,7 @@ int main() {
         int play = ShowMenu(mainscr);
         ClearScreenT(mainscr, GAME_TITLE);
 
-        if(!play) break;
+        if (!play) break;
         if (play == 80) {
             //ShowLeaderboard();
             continue;
@@ -31,46 +30,49 @@ int main() {
             config = ReadConfigFile(CONFIG_FILENAME);
             continue;
         }
-
         const game_t *game = StartGame(config, game_screen, play);
-
-        object_t frog = game->frog;
 
         ClearScreenT(mainscr, GAME_TITLE);
 
         DrawGround(mainscr, game->ground);
         ShowCountdown(mainscr);
 
-        const long starttime = GetTime();
         //GAME LOOP
-        while (1) {
-            // timeout(0);
-            wtimeout(mainwin, 1000/FPS);
-            int input = wgetch(mainwin);
-            if (input == 'q') break;
-
-            DrawGround(mainscr, game->ground);
-
-            frog.movement = IntToMove(input);
-            MoveFrog(mainscr, &frog);
-            //MoveCar();
-            //MoveLog();
-            //MoveStork();
-            //CheckCollision();
-
-            long gametime = GetTime() - starttime;
-
-            if(frog.pos.posy == 0) {
-                ShowFinish(mainscr, config, gametime);
-                break;
-            }
-
-            mvwprintw(mainwin, 0, mainscr.width - 12, "[%02ld:%02ld.%02ld]",
-                      gametime / 1000 / 60,
-                      (gametime / 1000) % 60,
-                      gametime % 1000);
-        }
+        GameLoop(mainscr, game, config);
     }
     endwin();
     return 0;
+}
+
+void GameLoop(const screen_t mainscr, const game_t *game, const config_t *config) {
+    WINDOW *mainwin = mainscr.win;
+    object_t frog = game->frog;
+
+    const long starttime = GetTime();
+    while (1) {
+        wtimeout(mainwin, 1000 / FPS);
+        int input = wgetch(mainwin);
+        if (input == 'q') break;
+
+        DrawGround(mainscr, game->ground);
+
+        frog.movement = IntToMove(input);
+        MoveFrog(mainscr, &frog);
+        //MoveCar();
+        //MoveLog();
+        //MoveStork();
+        //CheckCollision();
+
+        long gametime = GetTime() - starttime;
+
+        if (frog.pos.posy == 0) {
+            ShowFinish(mainscr, config, gametime);
+            break;
+        }
+
+        mvwprintw(mainwin, 0, mainscr.width - 12, "[%02ld:%02ld.%02ld]",
+                  gametime / 1000 / 60,
+                  (gametime / 1000) % 60,
+                  gametime % 1000);
+    }
 }
